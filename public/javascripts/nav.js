c5a87c9cf39c1add0e06a9df4a3ab34680565f3d
flanban <johnsfuller@gmail.com>
2015-07-28 17:36:18 -0400

    remove hardcoded height

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -1,21 +1,22 @@
 $(function(){
+  header = $('body > header')
   var prevScroll = 0,
       curDir = 'down',
       prevDir = 'up';
-  
+      headerHeight = header.outerHeight()
   $(window).scroll(function(){
     	if($(this).scrollTop() >= prevScroll){
           curDir = 'down';
           if(curDir != prevDir){
-          $('body > header').stop();
-    	  	$('body > header').animate({ top: '-50px' }, 300);
+          $(header).stop();
+    	  	$(header).animate({ top: -headerHeight }, 300);
           prevDir = curDir;
           }
       } else {
           curDir = 'up';
           if(curDir != prevDir){
-          $('body > header').stop();
-          $('body > header').animate({ top: '0px' }, 300);
+          $(header).stop();
+          $(header).animate({ top: '0px' }, 300);
           prevDir = curDir;
           }
       }
